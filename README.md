94563d4e985a5f4f3100f543cf9d7ae1059bb7bb
FBerthelot <florent.berthelot35@gmail.com>
2015-11-12 00:16:52 +0100

    style du contenu de la page

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-#poneyCase blog
+#La veilleuse blog
 
 [![Build Status](https://travis-ci.org/poneyCase/poneyCase.svg)](https://travis-ci.org/poneyCase/poneyCase)
+
+
+##Idées de noms
+Nextitude
+La veilleuse du net
+La veilleuse du web
+La veilleuse
+veillons !
+tu nextite
+n'extprime
+peer vert
--- build.js
+++ build.js
@@ -6,7 +6,50 @@ let markdown = require('metalsmith-markdownit');
 let layouts = require('metalsmith-layouts');
 let path = require('path');
 let moveUp = require('metalsmith-move-up');
+let hljs = require('highlight.js');
 
+let md = markdown({
+    typographer: true,
+    html: true,
+    linkify: true,
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return hljs.highlight(lang, str).value;
+            } catch (__) {}
+        }
+
+        try {
+            return hljs.highlightAuto(str).value;
+        } catch (__) {}
+
+        return ''; // use external default escaping
+    }
+});
+md.parser
+    .use(require('markdown-it-sub'))
+    .use(require('markdown-it-sup'))
+//    .use(require('markdown-it-footnote')) TODO reactiver les plugins quand ils marcherons, ouvrir une PR ?
+//    .use(require('markdown-it-deflist'))
+    .use(require('markdown-it-abbr'))
+/*    .use(require('markdown-it-container'), 'warrning', {
+    validate: function(params) {
+        return params.trim().match(/warning/);
+    },
+
+    render: function (tokens, idx) {
+        var m = tokens[idx].info.trim().match(/warning/);
+
+        if (tokens[idx].nesting === 1) {
+            // opening tag
+            return '<p class="warning">\n';
+
+        } else {
+            // closing tag
+            return '</p>\n';
+        }
+    }
+})*/;
 
 Metalsmith(__dirname)
     .source(path.join(__dirname, 'src'))
@@ -19,11 +62,11 @@ Metalsmith(__dirname)
         source: 'src/assets',
         destination: 'assets'
     }))
-    .use(markdown({
-        typographer: true,
-        html: true,
-        linkify: true
+    .use(assets({
+        source: 'node_modules/highlight.js/styles',
+        destination: 'styles'
     }))
+    .use(md)
     .use(layouts({
         'engine': 'handlebars',
         'directory': 'src',
